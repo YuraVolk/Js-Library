a5d8e2b672c83897bd2bd283137ea4cbf76ed3bd
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-08-09 13:05:47 +0300

    Carousel 3d Final

--- src/js/mainControl.js
+++ src/js/mainControl.js
@@ -599,19 +599,23 @@ markups = [
         });</code></pre>
     `,`
     <pre><code>
-    
+  
 window.addEventListener('load', () => {
     var carousel3d = document.querySelector('.carousel');
-    initCarousel(carousel3d);
-
+  
+    initCarousel(carousel3d, true);
 });
 
-function rotateCarousel(theta, imageIndex, figure) {
-    figure.style.transform = "rotateY(" + imageIndex * -theta + "rad)";
+function rotateCarousel(theta, imageIndex, figure, isntVertical) {
+    //figure.style.transform = "rotateX(" + imageIndex * -theta + "rad)"  VERTICAL CAROUSEL ONLY;
+    if (isntVertical) {  
+        figure.style.transform = "rotateY(" + imageIndex * -theta + "rad)";
+    } else {
+        figure.style.transform = "rotateX(" + imageIndex * -theta + "rad)";
+    }
 }
 
-
-function initCarousel(parent) {
+function initCarousel(parent, isntVertical) {
     let figure = parent.querySelector('figure');
     let nav = document.querySelector('.carousel-nav');
     let images = figure.children;
@@ -620,11 +624,23 @@ function initCarousel(parent) {
     let theta =  2 * Math.PI / n;
     let currImage = 0;
     
-    setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+    /*setupCarousel(n, parseFloat(getComputedStyle(images[0]).height));
     window.addEventListener('resize', () => { 
-        setupCarousel(n, parseFloat(getComputedStyle(images[0]).width)) 
-    });
-
+        setupCarousel(n, parseFloat(getComputedStyle(images[0]).height)) 
+    }); VERTICAL CAROUSEL ONLY*/
+   
+    if (isntVertical) {
+        setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+        window.addEventListener('resize', () => { 
+            setupCarousel(n, parseFloat(getComputedStyle(images[0]).width)) 
+        });
+    } else {
+        setupCarousel(n, parseFloat(getComputedStyle(images[0]).height));
+        window.addEventListener('resize', () => { 
+            setupCarousel(n, parseFloat(getComputedStyle(images[0]).height)) 
+        });
+    }
+    
     setupNavigation();
 
     function setupCarousel(n, s) {
@@ -636,14 +652,20 @@ function initCarousel(parent) {
             images[i].style.padding = "0";
         }for (i = 1; i < n; i++) {
             images[i].style.transformOrigin = "50% 50% " + -apothem + "px";
-            images[i].style.transform = "rotateY(" + i * theta + "rad)";
+            //images[i].style.transform = "rotateX(" + i * theta + "rad)"  VERTICAL CAROUSEL ONLY;
+            if (isntVertical) {
+                images[i].style.transform = "rotateY(" + i * theta + "rad)";
+            } else {
+                images[i].style.transform = "rotateX(" + i * theta + "rad)";
+            }
+            images[i].style.padding = "0";
         }
         
         for (i = 0; i < n; i++) {
             images[i].style.backfaceVisibility = 'hidden';
         }
         
-        rotateCarousel(theta, currImage, figure);
+        rotateCarousel(theta, currImage, figure, isntVertical);
     }
 
     function setupNavigation() {
@@ -659,17 +681,31 @@ function initCarousel(parent) {
             if (t.classList.contains('next')) {
                 currImage++;
             }
-            else {
+            else if (t.classList.contains('prev')) {
                 currImage--;
+            } else if (t.classList.contains('switch3d')) {
+                isntVertical = !isntVertical;
+                if (isntVertical) {
+                    parent.style.height = 'auto';
+                    figure.style.marginTop = 0;
+                } else {
+                    parent.style.height = '870px';
+                    figure.style.marginTop = '120px';
+                }
             }
             
-            rotateCarousel(theta, currImage, figure);
+            rotateCarousel(theta, currImage, figure, isntVertical);
         }
     }
-    
+
     setInterval(function() {
-        setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
-    }, 60);
+        //setupCarousel(n, parseFloat(getComputedStyle(images[0]).height))  VERTICAL CAROUSEL ONLY;
+        if (isntVertical) { 
+            setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+        } else {
+            setupCarousel(n, parseFloat(getComputedStyle(images[0]).height));
+        }
+    }, 160);
 
 }
     </code></pre>
