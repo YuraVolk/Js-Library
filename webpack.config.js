f8eda59ac7eb3afa1028c3ee3d5e9f801d90396f
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-08-23 17:17:02 +0300

    PWA Site Optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
     mode: 'development',
