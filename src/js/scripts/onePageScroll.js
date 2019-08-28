7faa69695c2278e693f1ce5fff5696e3cab78da9
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-08-28 13:44:53 +0300

    One Page Scroll

--- src/js/scripts/onePageScroll.js
+++ src/js/scripts/onePageScroll.js
@@ -17,33 +17,66 @@ function scrollDir(event) {
     }
 }
 
-function smoothScrollTo() {
+function easeInOutQuad(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+}
 
+function smoothScrollTo(element, to, duration) {
+    isScrolling = true;
+    var start = element.scrollTop,
+        change = to - start,
+        currentTime = 0,
+        increment = 10;
+
+    var animateScroll = function () {
+        currentTime += increment;
+        var val = easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if (currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        }
+        if (currentTime === duration) {
+           isScrolling = false;
+        }
+    };
+    animateScroll();
 }
 
 function scrollToSlide(dir) {
     if (!isScrolling) {
         if (dir == true) {
+            const toPixel = items[selectedItem + 1].offsetTop;
+            smoothScrollTo(items[0].parentNode, toPixel, 600);
             selectedItem++;
-            console.log(items[selectedItem]);
         } else {
+            const toPixel = items[selectedItem - 1].offsetTop;
+            smoothScrollTo(items[0].parentNode, toPixel, 600);
             selectedItem--;
-            console.log(items[selectedItem]);
         }
-    }   
+    }
 }
 
 document.querySelector('.scroll-select').addEventListener('wheel', (e) => {
     e.preventDefault();
     if (scrollDir(e)) {
         if (selectedItem >= items.length - 1) {
-            console.log('cant go down');
+            if (!isScrolling) {
+                selectedItem = 0;
+                smoothScrollTo(items[0].parentNode, items[0].offsetTop, 600);
+            }
         } else {
             scrollToSlide(true);
         }
     } else {
         if (selectedItem === 0) {
-            console.log('cant go up');
+            if (!isScrolling) {
+                const toPixel = items[0].parentNode.scrollHeight - items[0].offsetHeight;
+                selectedItem = items.length - 1;
+                smoothScrollTo(items[0].parentNode, toPixel, 600);
+            }
         } else {
             scrollToSlide(false);
         }
