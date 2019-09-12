994146d97572a710404cd11dd8626e445bdd06a6
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-09-12 13:17:06 +0300

    Table Multi Filtering

--- src/js/scripts/tableFiltering.js
+++ src/js/scripts/tableFiltering.js
@@ -5,6 +5,7 @@ const filtersEl = table.querySelector('.table-filters');
 const input = document.querySelector('.filtering-input');
 const tableTr = Array.prototype.slice.call(table.children[0].children);
 tableTr.shift();
+const select = document.querySelector('.select-filtering');
 
 let isShown = false;
 let selected = 'Name';
@@ -13,15 +14,23 @@ function showAll() {
   tableTr.forEach((el) => el.classList.remove('option--hidden'));
 }
 
+function filter() {
+  const num = filters.indexOf(selected);
+  tableTr.forEach((el) => {
+    if (el.children[num].textContent.toLowerCase().indexOf(input.value.toLowerCase()) === -1) {
+      el.classList.add('option--hidden');
+    }
+  });
+}
+
 Array.prototype.slice.call(filtersEl.children).forEach((el) => {
   filters.push(el.textContent.charAt(0).toUpperCase() + el.textContent.slice(1));
 });
 
-const select = document.querySelector('.select-filtering');
-
 filters.forEach((el) => {
-  select.insertAdjacentHTML('afterbegin', `<p class="option">${el}</p>`);
+  select.insertAdjacentHTML('beforeend', "<p class=\"option\">" + el + "</p>");
 });
+
 
 document.addEventListener('click', (e) => {
   if (e.target.classList[0] === 'chooser') {
@@ -37,9 +46,16 @@ document.addEventListener('click', (e) => {
     document.querySelector('.select-filtering').classList.add('option--hidden');
     selected = e.target.textContent;
     document.querySelector('.chooser').textContent = selected;
+    showAll();
+    if (input.value.trim().length !== 0) {
+      filter();
+    }
   }
 });
 
 input.addEventListener('input', (e) => {
   showAll();
+  if (input.value.trim().length !== 0) {
+    filter();
+  }
 });
