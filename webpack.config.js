4193ecb22d2d0532b3bb999aa167cc8ad6a81d37
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-09-19 16:29:00 +0300

    Brotli Optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -18,6 +20,19 @@ module.exports = {
       new HtmlWebpackPlugin({
           filename: 'index.html',
           template: './src/index.html',
+      }),
+      new CompressionPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.7
+      }),
+      new BrotliPlugin({
+        asset: '[path].br[query]',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.7
       })
     ],
     module: {
