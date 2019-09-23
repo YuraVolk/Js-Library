a9ee1bfdbb3319188ded01810d57740037d810e2
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-09-23 16:56:12 +0300

    Webpack Final Optimization

--- webpack.config.js
+++ webpack.config.js
@@ -3,55 +3,72 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const BrotliPlugin = require('brotli-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
-    entry: ['./src/js/loader.js'],
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'js/bundle.js'
-    },
-    devServer: {
-      contentBase: './dist',
-      disableHostCheck: true,
-      staticOptions: {
-        redirect: true
-      }
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-          filename: 'index.html',
-          template: './src/index.html',
-      }),
-      new CompressionPlugin({
-        filename: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: /\.js$|\.css$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.7
-      }),
-      new BrotliPlugin({
-        asset: '[path].br[query]',
-        test: /\.js$|\.css$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.7
-      })
-    ],
-    optimization: {
-      moduleIds: 'hashed',
-      chunkIds: 'total-size',
-      mangleWasmImports: true,
-      minimizer: [
-        new TerserPlugin({
-          test: /\.js(\?.*)?$/i,
-          terserOptions: {
-            mangle: true,
-            ie11: false,
-            ie8: false,
-            safari10: true
-          }
-        }),
-      ],
+  mode: 'development',
+  entry: [/*'@babel/polyfill', */'./src/js/loader.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    disableHostCheck: true,
+    staticOptions: {
+      redirect: true
     }
-  };
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.7
+    }),
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.7
+    })
+  ],
+  optimization: {
+    moduleIds: 'hashed',
+    chunkIds: 'total-size',
+    mangleWasmImports: true,
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        terserOptions: {
+          mangle: true,
+          ie11: false,
+          ie10: false,
+          ie9: false,
+          ie8: false,
+          safari10: true
+        }
+      })
+    ]
+  }/*,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }*/
+};
 
