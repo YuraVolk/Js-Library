817d830b78b32eefbf612774ff71ae833944fb2d
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-12-16 18:11:39 +0300

    Autocomplete List BEM

--- src/js/scripts/autocomplete.js
+++ src/js/scripts/autocomplete.js
@@ -3,12 +3,12 @@
 const data = {
   ID: 'autocomplete-list',
   CLASS: 'autocomplete-items',
-  ITEMS: 'item-set',
+  ITEMS: 'autocomplete-list__item',
   ITEM: 'autoInput'
 };
 
 const countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
-let autoInput = document.querySelector('.autocomplete-input');
+let autoInput = document.querySelector('.autocomplete__input');
 
 autoInput.addEventListener("input", function (e) {
   const val = this.value;
@@ -16,19 +16,19 @@ autoInput.addEventListener("input", function (e) {
   if (!val) {
     return false;
   }
-  this.parentNode.insertAdjacentHTML('afterbegin', `<div id="${data.ID}" class="${data.CLASS}"></div>`);
+  this.parentNode.insertAdjacentHTML('afterbegin', `<ul id="${data.ID}" class="${data.CLASS}"></ul>`);
   const el = document.querySelector('.autocomplete-items');
   for (var i = 0; i < countries.length; i++) {
     if (countries[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-    el.insertAdjacentHTML('beforeend', `<div class="${data.ITEMS}">\n<span><strong>` +
+    el.insertAdjacentHTML('beforeend', `<li class="${data.ITEMS}">\n<strong>` +
           countries[i].substr(0, val.length) + '</strong>' + countries[i].substr(val.length) +
-         `</span>\n<input class="${data.ITEM}" type="hidden" value="` + countries[i] + '">\n </div>');
+         `</li>`);
     }
   }
 });
 autoInput.addEventListener("keydown", function (e) {
   var x = document.getElementById(this.id + data.ID);
-  if (x) x = x.getElementsByTagName("div");
+  if (x) x = x.getElementsByTagName("li");
 });
 
 function closeAllLists(target) {
