b46529053b142c9d41dd0063a060b2558b2ad9f8
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-12-16 19:10:04 +0300

    Toast BEM

--- src/js/scripts/toast.js
+++ src/js/scripts/toast.js
@@ -3,9 +3,9 @@
 const data = {
   TOAST: 'toast-wrap',
   TOAST_INNER: 'toast',
-  TOAST_MRK: "<div class=\"toast\">\n    <div class=\"justify\">\n      <span>Some Toast Content Here</span>\n      <div class=\"flexbox-toast\">\n        <button class=\"modalButton closeToast\">OK</button>\n        <button class=\"modalButton closeToast\">Cancel</button>\n      </div>\n    </div>\n  </div>",
-  TOAST_OPEN_BTN: 'toastButton',
-  TOAST_CLOSE_BTN: 'closeToast'
+  TOAST_MRK: "<div class=\"toast\">\n    <div class=\"toast__content\">\n      <span>Some Toast Content Here</span>\n      <div class=\"toast__buttons\">\n        <button class=\"btn close-toast__button\">OK</button>\n        <button class=\"btn close-toast__button\">Cancel</button>\n      </div>\n    </div>\n  </div>",
+  TOAST_OPEN_BTN: 'open-toast__button',
+  TOAST_CLOSE_BTN: 'close-toast__button'
 };
 
 let isHidden = 0;
