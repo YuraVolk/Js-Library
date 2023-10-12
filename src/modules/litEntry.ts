92c08a620132e799e50d06920ae2ed072176a871
YuraVolk <yura.volkovskiy.2006@gmail.com>
2023-10-12 12:22:42 +0300

    logic for autocomplete list

--- src/modules/litEntry.ts
+++ src/modules/litEntry.ts
@@ -1,5 +1,7 @@
 import "./web-components/accordion";
 import "./web-components/backfaceCarousel";
 import "./web-components/menuCarousel";
+import "./web-components/autocompleteList";
+
 import "../docs/lit/sidebar";
 import "../docs/lit/header";
--- src/modules/web-components/autocompleteList.ts
+++ src/modules/web-components/autocompleteList.ts
@@ -6,7 +6,7 @@ export class AutocompleteListComponent extends LitElement {
   @property({ type: Array, converter: {
     fromAttribute: (value): string[] => {
       if (value === null) return [];
-      return value.split(",");
+      return value.split(",").map(e => e.trim());
     },
     toAttribute: (value: string[]): string => {
       return value.join(",");
@@ -14,27 +14,62 @@ export class AutocompleteListComponent extends LitElement {
   } })
   options: string[] = [];
 
-  private boundClickEventListener!: EventListener;
+  @state()
+  _isListOpened = false;
+  @state()
+  _filteredOptions: string[] = [];
+
   private boundInputEventListener!: EventListener;
 
-  protected onClickHandler(target: EventTarget) {
+  protected onInputHandler(target: EventTarget) {
+    if (!(target instanceof HTMLInputElement) || !target.value) {
+      this._isListOpened = false;
+      return;
+    }
+    const value = target.value.toLowerCase().trim();
+    const newFilteredOptions = [];
+    for (const option of this.options) {
+      const lowerOption = option.toLowerCase();
+      console.log(lowerOption.startsWith(value));
+      if (lowerOption === value || !lowerOption.startsWith(value)) continue;
+      newFilteredOptions.push(option);
+    }
 
+    if (newFilteredOptions.length) {
+      this._isListOpened = true;
+      this._filteredOptions = newFilteredOptions;
+    }
+  }
+
+  protected setInputValue(newValue: string) {
+    const input = this.querySelector<HTMLInputElement>("input");
+    if (input) {
+      input.value = newValue;
+      this._isListOpened = false;
+      this._filteredOptions = [];
+    }
   }
 
   connectedCallback() {
     super.connectedCallback();
-    this.boundClickEventListener = ({ target }) => { if (target) this.onClickHandler(target); };
-    this.addEventListener("click", this.boundClickEventListener);
+    this.boundInputEventListener = ({ target }) => { if (target) this.onInputHandler(target); };
+    console.log(this.querySelector("input"));
+    this.querySelector<HTMLInputElement>("input")?.addEventListener("input", this.boundInputEventListener);
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
-    this.removeEventListener("click", this.boundClickEventListener);
+    this.querySelector("input")?.removeEventListener("input", this.boundInputEventListener);
   }
 
   render() {
     return html`
       <slot></slot>
+      ${this._isListOpened && this._filteredOptions.length ? html`<ul class="autocomplete-items">
+        ${this._filteredOptions.map(option => {
+          return html`<li class="autocomplete-item" @click="${() => { this.setInputValue(option); }}">${option}</li>`;
+        })}
+      </ul>` : ""}
     `;
   }
 }
