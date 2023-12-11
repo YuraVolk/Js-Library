4118e1d5e7fe581c3c901b512bfaf1679d6198d8
YuraVolk <yura.volkovskiy.2006@gmail.com>
2023-12-11 19:49:26 +0300

    implement one page scroll in vue

--- src/docs/vue/one-page-scroll/App.vue
+++ src/docs/vue/one-page-scroll/App.vue
@@ -3,11 +3,60 @@
 	<sidebar-component active-link="One Page Scroll" />
 	<main class="main">
 		<h1 class="heading">One Page Scroll</h1>
-        <one-page-scroll-component class="one-page-scroll">
-            <one-page-scroll-item-component>
-
-            </one-page-scroll-item-component>
-        </one-page-scroll-component>
+		<p>
+			The one page scroll component provides a smooth scrolling experience for a single-page application. It allows users to navigate
+			through different sections of the page by scrolling vertically or horizontally. Its additional plus is that you do not need to depend
+			on custom animations, specific CSS styles. The internal formulas automatically adapt to most common scenarios.
+		</p>
+		<ul>
+			<li>
+				isHorizontal: A boolean property that determines whether the scrolling should be horizontal (true) or vertical (false). By default,
+				it is set to false.
+			</li>
+			<li>
+				duration: A number property that specifies the duration of the smooth scrolling animation in milliseconds. The default value is 500.
+			</li>
+			<li>
+				increment: A number property that determines the increment value for each step of the smooth scrolling animation. The default value
+				is 6.
+			</li>
+			<li>
+				noScrollbar: A boolean property that controls whether the scrollbars should be hidden (true) or visible (false). Enabled by default.
+			</li>
+		</ul>
+		<one-page-scroll-component class="one-page-scroll">
+			<one-page-scroll-item-component class="one-page-scroll-slide">
+				<p class="one-page-scroll-slide__heading">First slide. Scroll down...</p>
+			</one-page-scroll-item-component>
+			<one-page-scroll-item-component class="one-page-scroll-slide">
+				<p class="one-page-scroll-slide__heading">Second slide.</p>
+			</one-page-scroll-item-component>
+			<one-page-scroll-item-component class="one-page-scroll-slide">
+				<p class="one-page-scroll-slide__heading">Third slide.</p>
+			</one-page-scroll-item-component>
+			<one-page-scroll-item-component class="one-page-scroll-slide">
+				<p class="one-page-scroll-slide__heading">Fourth slide.</p>
+			</one-page-scroll-item-component>
+		</one-page-scroll-component>
+		<p>Here is an example of horizontal one-page scroll:</p>
+		<one-page-scroll-component class="one-page-scroll" is-horizontal>
+			<one-page-scroll-item-component class="one-page-scroll-slide">
+				<p class="one-page-scroll-slide__heading">First slide. Scroll down...</p>
+			</one-page-scroll-item-component>
+			<one-page-scroll-item-component class="one-page-scroll-slide">
+				<p class="one-page-scroll-slide__heading">Second slide.</p>
+			</one-page-scroll-item-component>
+			<one-page-scroll-item-component class="one-page-scroll-slide">
+				<p class="one-page-scroll-slide__heading">Third slide.</p>
+			</one-page-scroll-item-component>
+			<one-page-scroll-item-component class="one-page-scroll-slide">
+				<p class="one-page-scroll-slide__heading">Fourth slide.</p>
+			</one-page-scroll-item-component>
+		</one-page-scroll-component>
+		<p>
+			In order to get some default styling, it is highly recommended you use the local provided one page scroll item component along with
+			the one page scroll component in order to get the one page scroll work right away with minimum configuration.
+		</p>
 	</main>
 </template>
 
@@ -20,24 +69,24 @@ import SidebarComponent from "../SidebarComponent.vue";
 
 <style scoped>
 .one-page-scroll {
-  display: block;
-  width: 500px;
-  height: 500px;
-  border: 3px solid #333;
+	display: block;
+	width: 500px;
+	height: 500px;
+	border: 3px solid #333;
 }
 
 .one-page-scroll-slide {
-  min-width: 500px;
-  height: 500px;
-  box-sizing: border-box;
-  border: 3px solid #2390bb;
-  background-color: #62626253;
-  flex: 1 0 0;
+	min-width: 500px;
+	height: 500px;
+	box-sizing: border-box;
+	border: 3px solid #2390bb;
+	background-color: #62626253;
+	flex: 1 0 0;
 }
 
 .one-page-scroll-slide__heading {
-  margin: 0;
-  font-size: 32px;
-  line-height: 54px;
+	margin: 0;
+	font-size: 32px;
+	line-height: 54px;
 }
 </style>
