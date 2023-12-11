6fb02e1f2bdbb348951c2142fa0e8cbd4db00726
YuraVolk <yura.volkovskiy.2006@gmail.com>
2023-12-11 19:49:26 +0300

    implement one page scroll in vue

--- src/modules/web-components/one-page-scroll/onePageScroll.ts
+++ src/modules/web-components/one-page-scroll/onePageScroll.ts
@@ -1,7 +1,7 @@
 import { LitElement, css, html } from "lit";
 import { customElement, property, query, queryAssignedElements, state } from "lit/decorators.js";
 import { classMap } from "lit/directives/class-map.js";
-import { OnePageScrollConfiguration } from "src/modules/interfaces/component/one-page-scroll/types";
+import { OnePageScrollConfiguration, easeInOutQuad } from "src/modules/interfaces/component/one-page-scroll/types";
 import { CarouselDirection } from "src/modules/interfaces/generic/carousel/carousel";
 
 declare global {
@@ -28,9 +28,9 @@ export class OnePageScrollComponent extends LitElement implements OnePageScrollC
 			overflow-y: hidden;
 		}
 
-        .wrap--no-scrollbar {
-            overflow: hidden;
-        }
+		.wrap--no-scrollbar {
+			overflow: hidden;
+		}
 	`;
 
 	@property({ type: Boolean })
@@ -39,8 +39,8 @@ export class OnePageScrollComponent extends LitElement implements OnePageScrollC
 	duration = 500;
 	@property({ type: Number })
 	increment = 6;
-    @property({ type: Boolean })
-    noScrollbar = false;
+	@property({ type: Boolean })
+	noScrollbar = false;
 
 	@state()
 	_selectedItem = 0;
@@ -54,42 +54,37 @@ export class OnePageScrollComponent extends LitElement implements OnePageScrollC
 	@query(".wrap")
 	_wrapElement!: HTMLDivElement;
 
-	private easeInOutQuad(t: number, b: number, c: number, d: number) {
-		t /= d / 2;
-		if (t < 1) return (c / 2) * t * t + b;
-		t--;
-		return (-c / 2) * (t * (t - 2) - 1) + b;
-	}
-
 	protected smoothScrollTo(to: number) {
 		this._isScrolling = true;
 		const wrap = this._wrapElement;
 		const start = this.isHorizontal ? wrap.scrollLeft : wrap.scrollTop,
 			change = to - start;
 		let currentTime = 0;
-        const property = this.isHorizontal ? "scrollLeft" : "scrollTop";
+		const property = this.isHorizontal ? "scrollLeft" : "scrollTop";
 
 		const animateScroll = () => {
 			if (!this.scrollEventListener) return;
-            currentTime += this.increment;
-            const quad = this.easeInOutQuad(currentTime, start, change, this.duration);
-            if (quad > this.increment) {
-                wrap[property] = quad - (this.increment / 2);
-            } else wrap[property] = quad;
+			currentTime += this.increment;
+			const quad = easeInOutQuad(currentTime, start, change, this.duration);
+			if (quad > this.increment) {
+				wrap[property] = quad - this.increment / 2;
+			} else wrap[property] = quad;
 			if (currentTime < this.duration) {
 				setTimeout(animateScroll, this.increment);
 			} else if (currentTime >= this.duration) {
-                wrap[property] = to;
-                this._isScrolling = false;
-            }
+				wrap[property] = to;
+				this._isScrolling = false;
+			}
 		};
 		animateScroll();
 	}
 
-	protected scrollSlide(direction: CarouselDirection.BACKWARDS | CarouselDirection.FORWARDS) {
+	protected scrollSlide(direction: CarouselDirection) {
 		if (this._isScrolling) return;
-        this._selectedItem += direction;
-		this.smoothScrollTo(this._onePageScrollElements[this._selectedItem].offsetHeight * this._selectedItem);    
+		this._selectedItem += direction;
+		this.smoothScrollTo(
+			this._onePageScrollElements[this._selectedItem][this.isHorizontal ? "offsetWidth" : "offsetHeight"] * this._selectedItem
+		);
 	}
 
 	protected firstUpdated() {
@@ -99,7 +94,7 @@ export class OnePageScrollComponent extends LitElement implements OnePageScrollC
 			(this.scrollEventListener = (event) => {
 				if (event.deltaY > 0) {
 					if (this._selectedItem >= items.length - 1) {
-						if (!this._isScrolling) this.smoothScrollTo(this._selectedItem = 0);
+						if (!this._isScrolling) this.smoothScrollTo((this._selectedItem = 0));
 					} else this.scrollSlide(CarouselDirection.FORWARDS);
 				} else {
 					if (this._selectedItem === 0) {
@@ -113,7 +108,7 @@ export class OnePageScrollComponent extends LitElement implements OnePageScrollC
 					} else this.scrollSlide(CarouselDirection.BACKWARDS);
 				}
 			}),
-            { passive: true }
+			{ passive: true }
 		);
 	}
 
