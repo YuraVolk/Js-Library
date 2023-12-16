22740da5673e25e73180c5cf7500d72b9ac6a2eb
YuraVolk <yura.volkovskiy.2006@gmail.com>
2023-12-16 13:22:58 +0300

    implement rebuilding text in vue

--- src/modules/web-components/rebuilding-text/rebuildingText.ts
+++ src/modules/web-components/rebuilding-text/rebuildingText.ts
@@ -1,4 +1,5 @@
 import { customElement, property } from "lit/decorators.js";
+import { createRebuildingTextSteps } from "src/modules/interfaces/component/rebuilding-text/types";
 import { SelfModifyingText } from "src/modules/interfaces/generic/selfModifyingText/selfModifyingText.lit";
 
 declare global {
@@ -15,46 +16,17 @@ export class RebuildingTextComponent extends SelfModifyingText {
 	typingSpeed = 75;
 
 	triggerTextAnimation(fromText: string, toText: string): void {
-		const fromTextArray: Array<string | undefined> = fromText.split("");
-		const toTextArray: Array<string | undefined> = toText.split("");
-		if (fromTextArray.length < toTextArray.length) {
-			let index = 0;
-			while (fromTextArray.length < toText.length) {
-				if (++index === fromTextArray.length) index = 0;
-				fromTextArray.splice(index, 0, undefined);
-			}
-		} else if (toTextArray.length < fromTextArray.length) {
-			let index = 0;
-			while (toTextArray.length < fromTextArray.length) {
-				toTextArray.splice(index, 0, undefined);
-				if (++index === toTextArray.length) index = 0;
-			}
-		}
-
-		const steps: Array<string | undefined>[] = [fromTextArray];
-		const furtherQueue: number[] = [];
-		for (let i = 0; i < fromTextArray.length - 1; i++) {
-			const currentState = [...steps[steps.length - 1]];
-			if (i % 2 === 0) {
-				[currentState[i], currentState[i + 1]] = [toTextArray[i], currentState[i]];
-			} else furtherQueue.push(i);
-			if (furtherQueue.includes(i - 2)) {
-				const index = furtherQueue.shift() ?? 0;
-				currentState[index] = toTextArray[index];
-			}
-
-			steps.push(currentState);
-		}
-		steps.push(toTextArray);
+		const steps = createRebuildingTextSteps(fromText, toText);
 
 		for (let i = 0; i < steps.length; i++) {
 			setTimeout(() => {
 				for (const element of this._elements) {
 					element.textContent = steps[i].filter(Boolean).join("");
-					if (i === steps.length - 1)
-						setTimeout(() => {
-							this.onInterval();
-						}, this.interval);
+					if (i !== steps.length - 1) continue;
+
+					setTimeout(() => {
+						this.onInterval();
+					}, this.interval);
 				}
 			}, this.typingSpeed * i);
 		}
