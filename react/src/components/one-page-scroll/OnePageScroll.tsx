dadae9837575472113699db7ca8fd0ee0982dd78
YuraVolk <yura.volkovskiy.2006@gmail.com>
2024-01-17 20:49:35 +0300

    implement one page scroll component

---
+++ react/src/components/one-page-scroll/OnePageScroll.tsx
@@ -0,0 +1,108 @@
+import React, { useCallback, useEffect, useRef } from "react";
+import { ExposedContextFunctions, ContextLinkedItems } from "../../interfaces/hooks/useLinkedItem";
+import { easeInOutQuad } from "shared/component/onePageScroll";
+import { OnePageScrollConfiguration } from "../../interfaces/component/onePageScroll";
+import styles from "./OnePageScroll.module.css";
+import { CarouselDirection } from "shared/interfaces/carousel";
+
+export const OnePageScroll = ({
+	isHorizontal = false,
+	noScrollbar = true,
+	increment = 6,
+	duration = 500,
+	...props
+}: OnePageScrollConfiguration) => {
+	const linkedItemsContext = useRef<ExposedContextFunctions | null>(null);
+	const wrap = useRef<HTMLDivElement | null>(null);
+	const animationTimeout = useRef<{ timer?: number }>({ timer: undefined });
+	const selectedItem = useRef(0);
+	const isScrolling = useRef(false);
+
+	const smoothScrollTo = useCallback((to: number) => {
+		if (!wrap.current) return;
+		isScrolling.current = true;
+		const property = isHorizontal ? "scrollLeft" : "scrollTop";
+		const start = wrap.current[property],
+			change = to - start;
+		let currentTime = 0;
+
+        const animateScroll = () => {
+            if (!wrap.current) return;
+            currentTime += increment;
+            const quad = easeInOutQuad(currentTime, start, change, duration);
+            if (quad > increment) {
+                wrap.current[property] = quad - increment / 2;
+            } else wrap.current[property] = quad;
+    
+            if (currentTime < duration) {
+                animationTimeout.current.timer = window.setTimeout(animateScroll, increment);
+            } else if (currentTime >= duration) {
+                wrap.current[property] = to;
+                isScrolling.current = false;
+            }
+        };
+        
+        animateScroll();    
+	}, [duration, increment, isHorizontal]);
+
+	const scrollSlide = useCallback(
+		(direction: CarouselDirection) => {
+			const element = Object.values(linkedItemsContext.current?.getState() ?? {})[selectedItem.current]?.element?.current;
+			if (isScrolling.current || !element) return;
+			selectedItem.current += direction;
+
+			smoothScrollTo(element[isHorizontal ? "offsetWidth" : "offsetHeight"] * selectedItem.current);
+		},
+		[isHorizontal, smoothScrollTo]
+	);
+
+	const onWheel = useCallback(
+		(event: React.WheelEvent<HTMLDivElement>) => {
+			const elements = linkedItemsContext.current?.getState();
+			if (!elements) return;
+			const elementsLength = Object.keys(elements).length;
+			const firstElement = Object.values(elements)[0].element.current;
+			if (!firstElement) return;
+
+			if (event.deltaY > 0) {
+				if (selectedItem.current >= elementsLength - 1) {
+					if (isScrolling.current) return;
+					selectedItem.current = 0;
+					smoothScrollTo(0);
+				} else scrollSlide(CarouselDirection.FORWARDS);
+			} else {
+				if (selectedItem.current === 0) {
+					if (isScrolling.current || !wrap.current) return;
+					selectedItem.current = elementsLength - 1;
+					smoothScrollTo(
+						(isHorizontal ? wrap.current.scrollWidth : wrap.current.scrollHeight) -
+							(isHorizontal ? firstElement.offsetWidth : firstElement.offsetHeight)
+					);
+				} else scrollSlide(CarouselDirection.BACKWARDS);
+			}
+		},
+		[isHorizontal, scrollSlide, smoothScrollTo]
+	);
+
+	useEffect(() => {
+		const animationTimeoutObject = animationTimeout.current;
+
+		return () => {
+			window.clearTimeout(animationTimeoutObject.timer);
+		};
+	}, []);
+
+	return (
+		<ContextLinkedItems ref={linkedItemsContext} innerChildren={props.children}>
+			<article className={props.className}>
+				<div
+					ref={wrap}
+					onWheel={onWheel}
+					className={`${styles.wrap} ${isHorizontal ? styles["wrap--horizontal"] : ""} ${noScrollbar ? styles["wrap--no-scrollbar"] : ""}`}
+				>
+					{props.children}
+				</div>
+			</article>
+		</ContextLinkedItems>
+	);
+};
---
+++ react/src/docs/react/one-page-scroll/Page.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { OnePageScroll } from "../../../components/one-page-scroll/OnePageScroll";
+import styles from "./Page.module.css";
+import { OnePageScrollItem } from "../../../components/one-page-scroll/OnePageScrollItem";
+
+const Page = () => {
+	return (
+		<main className="main">
+			<h1 className="heading">One Page Scroll</h1>
+			<p>
+				The one page scroll component provides a smooth scrolling experience for a single-page application. It allows users to navigate
+				through different sections of the page by scrolling vertically or horizontally. Its additional plus is that you do not need to
+				depend on custom animations, specific CSS styles. The internal formulas automatically adapt to most common scenarios.
+			</p>
+			<ul>
+				<li>
+					isHorizontal: A boolean property that determines whether the scrolling should be horizontal (true) or vertical (false). By
+					default, it is set to false.
+				</li>
+				<li>
+					duration: A number property that specifies the duration of the smooth scrolling animation in milliseconds. The default value is
+					500.
+				</li>
+				<li>
+					increment: A number property that determines the increment value for each step of the smooth scrolling animation. The default
+					value is 6.
+				</li>
+				<li>
+					noScrollbar: A boolean property that controls whether the scrollbars should be hidden (true) or visible (false). Enabled by
+					default.
+				</li>
+			</ul>
+			<OnePageScroll className={styles["one-page-scroll"]}>
+				<OnePageScrollItem className={styles["one-page-scroll-slide"]}>
+					<p className={styles["one-page-scroll-slide__heading"]}>First slide. Scroll down...</p>
+				</OnePageScrollItem>
+				<OnePageScrollItem className={styles["one-page-scroll-slide"]}>
+					<p className={styles["one-page-scroll-slide__heading"]}>Second slide</p>
+				</OnePageScrollItem>
+				<OnePageScrollItem className={styles["one-page-scroll-slide"]}>
+					<p className={styles["one-page-scroll-slide__heading"]}>Third slide.</p>
+				</OnePageScrollItem>
+				<OnePageScrollItem className={styles["one-page-scroll-slide"]}>
+					<p className={styles["one-page-scroll-slide__heading"]}>Fourth slide.</p>
+				</OnePageScrollItem>
+			</OnePageScroll>
+			<p>Here is an example of horizontal one-page scroll:</p>
+			<OnePageScroll className={styles["one-page-scroll"]} isHorizontal>
+				<OnePageScrollItem className={styles["one-page-scroll-slide"]}>
+					<p className={styles["one-page-scroll-slide__heading"]}>First slide. Scroll down...</p>
+				</OnePageScrollItem>
+				<OnePageScrollItem className={styles["one-page-scroll-slide"]}>
+					<p className={styles["one-page-scroll-slide__heading"]}>Second slide</p>
+				</OnePageScrollItem>
+				<OnePageScrollItem className={styles["one-page-scroll-slide"]}>
+					<p className={styles["one-page-scroll-slide__heading"]}>Third slide.</p>
+				</OnePageScrollItem>
+				<OnePageScrollItem className={styles["one-page-scroll-slide"]}>
+					<p className={styles["one-page-scroll-slide__heading"]}>Fourth slide.</p>
+				</OnePageScrollItem>
+			</OnePageScroll>
+			<p>
+				In order to get some default styling, it is highly recommended you use the local provided one page scroll item component along with
+				the one page scroll component in order to get the one page scroll work right away with minimum configuration.
+			</p>
+		</main>
+	);
+};
+
+export default Page;
