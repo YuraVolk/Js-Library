93639394758718e69c369f235850de43be9ef378
Yuriy Volkovskiy <42916748+YuraVolk@users.noreply.github.com>
2024-01-18 19:25:42 +0300

    implement parallax component (#213)

---
+++ react/src/interfaces/component/parallax.ts
@@ -0,0 +1,7 @@
+import { ParallaxConfiguration as RootConfiguration } from "shared/component/parallax";
+import { GenericReactComponentProps } from "../generic/classNameFallthrough";
+import { ReactNode } from "react";
+
+export interface ParallaxConfiguration extends RootConfiguration, GenericReactComponentProps {
+    children: ReactNode;
+}
