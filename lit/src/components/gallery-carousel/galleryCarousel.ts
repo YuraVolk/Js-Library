6ff8dbb065f84195860ed33f74892bc7cd63d678
YuraVolk <yura.volkovskiy.2006@gmail.com>
2024-03-17 17:41:23 +0300

    test

--- lit/src/components/gallery-carousel/galleryCarousel.ts
+++ lit/src/components/gallery-carousel/galleryCarousel.ts
@@ -139,6 +139,7 @@ export class GalleryCarouselComponent extends LinkedCarouselMixin(LitElement) im
 	}
 
 	protected slideTo(position: number, newPosition: number, currentSlide: number) {
+		
 		this._isAnimating = true;
 		const start = new Date();
 		this._interval = window.setInterval(() => {
