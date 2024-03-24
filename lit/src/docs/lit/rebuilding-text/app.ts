21f76f84bc4fd8da0a01bea2cf7fb484cc8337ac
YuraVolk <yura.volkovskiy.2006@gmail.com>
2024-03-24 14:18:17 +0300

    refactor rebuilding text

--- lit/src/docs/lit/rebuilding-text/app.ts
+++ lit/src/docs/lit/rebuilding-text/app.ts
@@ -2,6 +2,8 @@ import type { RebuildingTextComponent } from "lit/src/components/rebuilding-text
 import "../../../components/litEntry";
 import "../../../global.css";
 import "./style.css";
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
 
 import("../header");
 import("../sidebar");
@@ -13,8 +15,37 @@ import("../../../components/rebuilding-text/rebuildingText")
 		console.trace(e);
 	});
 
+@customElement("rebuilding-text-application-example-1")
+export class RebuildingTextApplicationExample1 extends LitElement {
+	render() {
+		return html`<rebuilding-text-component .strings=${["Test text", "The new resulting text", "Small"]}></rebuilding-text-component>`;
+	}
+}
+
+@customElement("rebuilding-text-application-example-2")
+export class RebuildingTextApplicationExample2 extends LitElement {
+	render() {
+		return html`<rebuilding-text-component .strings=${["Hello", "World"]}></rebuilding-text-component>`;
+	}
+}
+
+@customElement("rebuilding-text-application-example-3")
+export class RebuildingTextApplicationExample3 extends LitElement {
+	render() {
+		return html`
+			<rebuilding-text-component
+				.strings=${["Test text", "The new resulting text", "Small"]}
+				.typingSpeed=${200}
+			></rebuilding-text-component>
+		`;
+	}
+}
+
 declare global {
 	interface HTMLElementTagNameMap {
 		"rebuilding-text-component": RebuildingTextComponent;
+		"rebuilding-text-application-example-1": RebuildingTextApplicationExample1;
+		"rebuilding-text-application-example-2": RebuildingTextApplicationExample2;
+		"rebuilding-text-application-example-3": RebuildingTextApplicationExample3;
 	}
 }
