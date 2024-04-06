245457ca88738fedc3300ef8e1eeb066f0fa821a
YuraVolk <yura.volkovskiy.2006@gmail.com>
2024-04-06 21:29:54 +0300

    implement transition controller in lit

---
+++ lit/src/interfaces/hooks/transitionController.ts
@@ -0,0 +1,144 @@
+import { LitElement, ReactiveController, noChange, nothing } from "lit";
+import { AsyncDirective, Directive, Part, PartInfo, PartType, directive } from "lit/async-directive.js";
+
+enum TransitionState {
+	ENTERING,
+	ENTERED,
+	EXITING,
+	EXITED
+}
+
+interface TransitionDirectiveOptions {
+	controller: TransitionController;
+	transitionState: TransitionState;
+}
+
+class TransitionDirective extends AsyncDirective {
+	private previousState?: TransitionState;
+
+	constructor(partInfo: PartInfo) {
+		super(partInfo);
+		if (partInfo.type !== PartType.ATTRIBUTE || partInfo.name !== "class") {
+			throw new Error("The `transition` directive must be used in the `class` attribute");
+		}
+	}
+
+	render({ transitionState }: TransitionDirectiveOptions) {
+		if (this.previousState === transitionState) {
+			return noChange;
+		}
+
+		switch (transitionState) {
+			case TransitionState.ENTERING:
+				return "enter-active enter";
+			case TransitionState.EXITING:
+				return "leave-active leave";
+			case TransitionState.ENTERED:
+				return "enter-done";
+			case TransitionState.EXITED:
+				return "leave-done";
+		}
+	}
+
+	update(_part: Part, [options]: Parameters<typeof this.render>) {
+		const { transitionState, controller } = options;
+		if (_part.type !== PartType.ATTRIBUTE || !(_part.element instanceof HTMLElement)) {
+			return this.render(options);
+		}
+
+		const element = _part.element;
+		switch (transitionState) {
+			case TransitionState.ENTERING:
+				element.classList.add("enter-active");
+				element && element.scrollTop;
+				controller.scheduleNextState(TransitionState.ENTERED);
+				break;
+			case TransitionState.ENTERED:
+				element.classList.remove("enter-active");
+				break;
+			case TransitionState.EXITING:
+				element.classList.add("leave-active");
+				element && element.scrollTop;
+				controller.scheduleNextState(TransitionState.EXITED);
+				break;
+			case TransitionState.EXITED:
+				element.classList.remove("leave-active");
+				break;
+		}
+
+		return this.render(options);
+	}
+}
+
+interface TransitionDisplayDirectiveOptions extends TransitionDirectiveOptions {
+	template: unknown;
+}
+
+class TransitionDisplayDirective extends Directive {
+	render({ template, transitionState }: TransitionDisplayDirectiveOptions) {
+		return transitionState === TransitionState.EXITED ? nothing : template;
+	}
+}
+
+export class TransitionController implements ReactiveController {
+	transitionState?: TransitionState;
+	private timeoutId?: number;
+
+	constructor(
+		private readonly host: LitElement,
+		private readonly duration: number,
+		private readonly isVisible: () => boolean
+	) {
+		host.addController(this);
+	}
+
+	hostConnected(): void {
+		this.transitionState = this.isVisible() ? TransitionState.ENTERED : TransitionState.EXITED;
+	}
+
+	hostDisconnected(): void {
+		window.clearTimeout(this.timeoutId);
+	}
+
+	setTransitionState(transitionState: TransitionState) {
+		this.transitionState = transitionState;
+		this.host.requestUpdate();
+	}
+
+	scheduleNextState(transitionState: TransitionState) {
+		this.timeoutId = window.setTimeout(() => {
+			this.setTransitionState(transitionState);
+		}, this.duration);
+	}
+
+	transitionDirective() {
+		if (typeof this.transitionState === "undefined") return nothing;
+
+		const isVisible = this.isVisible();
+		if (isVisible && this.transitionState === TransitionState.EXITED) {
+			this.transitionState = TransitionState.ENTERING;
+		} else if (!isVisible && this.transitionState === TransitionState.ENTERED) {
+			this.transitionState = TransitionState.EXITING;
+		}
+
+		return transitionDirective({
+			controller: this,
+			transitionState: this.transitionState
+		});
+	}
+
+	transitionDisplayDirective(template: unknown) {
+		if (typeof this.transitionState === "undefined") return nothing;
+
+		return transitionDisplayDirective({
+			controller: this,
+			transitionState: this.transitionState,
+			template
+		});
+	}
+}
+
+export const transitionDirective = directive(TransitionDirective);
+export const transitionDisplayDirective = directive(TransitionDisplayDirective);
+
+export type { TransitionDirective, TransitionDisplayDirective };
