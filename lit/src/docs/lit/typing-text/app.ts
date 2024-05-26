a595915de1c226146415ac967011a9f20c2601b8
YuraVolk <yura.volkovskiy.2006@gmail.com>
2024-05-26 17:23:25 +0300

    make ciphering text use scoped slots pattern

--- lit/src/docs/lit/typing-text/app.ts
+++ lit/src/docs/lit/typing-text/app.ts
@@ -4,31 +4,13 @@ import "../../../global.css";
 import "./style.css";
 import { LitElement, css, html } from "lit";
 import { customElement } from "lit/decorators.js";
+import type { SelfModifyingTextRenderer } from "lit/src/interfaces/generic/selfModifyingText";
 
 import("../header");
 import("../sidebar");
 import("../../../components/typing-text/typingText")
 	.then(({ TypingTextComponent }) => {
 		window.customElements.define("typing-text-component", TypingTextComponent);
-		window.customElements.define(
-			"typing-text-animated-component",
-			class extends TypingTextComponent {
-				static styles = [
-					...TypingTextComponent.styles,
-					css`
-						@keyframes color {
-							50% {
-								color: #57bb9d;
-							}
-						}
-
-						span {
-							animation: 0.5s color infinite;
-						}
-					`
-				];
-			}
-		);
 	})
 	.catch((e: unknown) => {
 		console.trace(e);
@@ -69,23 +51,37 @@ export class TypingTextApplicationExample2 extends LitElement {
 			line-height: 54px;
 			min-height: 5lh;
 		}
+
+		.typing-text > span {
+			animation: 0.5s color infinite;
+		}
+
+		@keyframes color {
+			50% {
+				color: #57bb9d;
+			}
+		}
 	`;
 	render() {
-		return html`<typing-text-animated-component
+		return html`<typing-text-component
 			class="typing-text"
 			.strings=${[
 				"",
 				"Nulla facilisi. Cras venenatis nulla non erat luctus ultrices. In facilisis tincidunt tristique. Curabitur lobortis sapien a metus lobortis, et lobortis nunc suscipit. Fusce orci mauris, bibendum et vehicula ac, fringilla at elit.",
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, atque nam! Perferendis non, deleniti tempore soluta magnam expedita nulla? Est repudiandae error rerum dolorum dolor ducimus ex tempore aut temporibus."
 			]}
-		></typing-text-animated-component>`;
+			.renderElements=${((letter, index) => ({
+				key: String(index),
+				value: html`<span>${letter.letter}</span>`
+			})) satisfies SelfModifyingTextRenderer}
+		>
+		</typing-text-component>`;
 	}
 }
 
 declare global {
 	interface HTMLElementTagNameMap {
 		"typing-text-component": TypingTextComponent;
-		"typing-text-animated-component": TypingTextComponent;
 		"typing-text-application-example-1": TypingTextApplicationExample1;
 		"typing-text-application-example-2": TypingTextApplicationExample2;
 	}
