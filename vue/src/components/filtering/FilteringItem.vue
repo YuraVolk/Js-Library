a9907e13675f2c4e773973be137a241a221acf2c
YuraVolk <yura.volkovskiy.2006@gmail.com>
2024-06-19 10:41:48 +0300

    setup base context

---
+++ vue/src/components/filtering/FilteringItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <slot v-if="filter === activeFilter" />
+</template>
+
+<script setup lang="ts">
+import { Ref, inject, toRef } from 'vue';
+import { activeFilterKey } from '../../interfaces/component/filtering';
+
+defineProps<{
+    filter: unknown;
+}>();
+
+const activeFilter = toRef(inject<Ref<unknown>>(activeFilterKey));
+</script>
