c87d4d0f268538e008536c6d01535ee82311e223
YuraVolk <yura.volkovskiy.2006@gmail.com>
2024-06-19 10:41:48 +0300

    setup base context

---
+++ vue/src/components/filtering/FilteringWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <slot name="filters" />
+  <TransitionGroup>
+    <slot />
+  </TransitionGroup>
+</template>
+
+<script setup lang="ts" generic="T">
+import { provide, ref } from 'vue';
+import { activeFilterKey } from "../../interfaces/component/filtering"
+
+const props = defineProps<{
+  filters: T[];
+  defaultFilter: T;
+}>();
+
+const activeFilter = ref(props.defaultFilter)
+
+provide(activeFilterKey, activeFilter);
+</script>
---
+++ vue/src/interfaces/component/filtering.ts
@@ -0,0 +1,1 @@
+export const activeFilterKey = Symbol();
