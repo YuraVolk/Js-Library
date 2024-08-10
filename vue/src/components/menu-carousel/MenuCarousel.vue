61a2651be75a96cb319f4ba89ef91f1a5d60b6f1
YuraVolk <yura.volkovskiy.2006@gmail.com>
2024-08-10 16:01:43 +0300

    setup improved typings for menu carousel in vue

--- vue/src/components/menu-carousel/MenuCarousel.vue
+++ vue/src/components/menu-carousel/MenuCarousel.vue
@@ -1,14 +1,43 @@
 <template>
   <ul ref="carousel">
     <slot />
-    <li class="carousel-controls">
+    <li
+      v-if="allowSwitchingOrientation || showArrows"
+      class="carousel-controls"
+    >
       <button
+        v-if="showArrows"
         class="carousel-controls__previous-button"
-        @click="goBack"
+        @click="onNextSlide"
       />
       <button
+        v-if="allowSwitchingOrientation"
+        class="carousel-controls__perspective-button"
+        @click="switchOrientation"
+      >
+        Switch
+      </button>
+      <button
+        v-if="showArrows"
         class="carousel-controls__next-button"
-        @click="goForward"
+        @click="onPreviousSlide"
+      />
+    </li>
+    <li
+      v-if="showToggles"
+      class="carousel-toggles carousel-controls__toggles"
+    >
+      <div
+        v-for="i in currentItemSettings.childrenLength"
+        :key="i"
+        class="carousel-controls__toggle"
+        :class="{ 'carousel-controls__toggle--active': currentItemSettings.realCurrentItem === i - 1 }"
+        @click="() => {
+          abortTimeout();
+          const difference = (currentItemSettings.getCurrentItem(i - 1) % currentItemSettings.childrenLength) - (currentItem % currentItemSettings.childrenLength);
+          go(difference);
+          currentItem += difference;
+        }"
       />
     </li>
   </ul>
@@ -18,23 +47,55 @@
 import { MenuCarouselConfiguration, MenuCarouselInternalItem } from 'shared/component/menuCarousel';
 import { useInjectedLinkedItems, LinkedVueItem } from '../../interfaces/hooks/useLinkedItem';
 import { assertNonUndefinedDevOnly } from "shared/utils/utils";
-import { onMounted, onUnmounted, reactive, ref } from 'vue';
+import { computed, onMounted, onUnmounted, reactive, ref } from 'vue';
 import { CarouselDirection } from 'shared/interfaces/carousel';
+import { getCurrentItemInfo } from "shared/utils/carousel";
+import { useAutoplay } from "../../interfaces/hooks/useAutoplay";
 
 const props = withDefaults(defineProps<MenuCarouselConfiguration>(), {
     yPos: 112,
     yRadius: 128,
     farScale: 0.9,
-    speed: 0.11
+    speed: 0.11,
+    showArrows: true,
+    showToggles: false,
+    allowSwitchingOrientation: false,
+    isVertical: false
 });
 
 const rotation = ref(Math.PI / 2);
 const destRotation = ref(Math.PI / 2);
 const frameTimer = ref<number | undefined>();
 const xRadius = ref(props.xRadius);
-const yRadius = ref(props.yRadius);
+const yRadius = ref<number | undefined>(props.yRadius);
 const xPos = ref(props.xPos);
 const yPos = ref(props.yPos);
+const currentItem = ref(0);
+const isVertical = ref(props.isVertical);
+const currentItemSettings = computed(() => {
+    return getCurrentItemInfo({ childrenLength: Object.keys(elements).length, currentItem: currentItem.value });
+});
+
+const orientationXRadius = computed({
+    get() {
+        return isVertical.value ? yRadius.value : xRadius.value
+    },
+    set(newValue: number | undefined) {
+        if (isVertical.value) {
+            yRadius.value = newValue;
+        } else xRadius.value = newValue;
+    }
+});
+const orientationYRadius = computed({
+    get() {
+        return isVertical.value ? xRadius.value : yRadius.value
+    },
+    set(newValue: number | undefined) {
+        if (isVertical.value) {
+            xRadius.value = newValue;
+        } else yRadius.value = newValue;
+    }
+});
 
 const carousel = ref<HTMLElement | null>(null);
 const elements = useInjectedLinkedItems();
@@ -70,7 +131,7 @@ const rotateItem = (itemIndex: number, rotation: number) => {
     assertNonUndefinedDevOnly(xRadius.value);
     item.moveTo(
         xPos.value + scale * (Math.cos(rotation) * xRadius.value - item.fullWidth / 2),
-        yPos.value + scale * sin * yRadius.value + yPos.value / 2.3,
+        yPos.value + scale * sin * (yRadius.value ?? 1) + yPos.value / 2.3,
         scale
     );
 };
@@ -114,8 +175,8 @@ const setupCarousel = () => {
     if (!carousel.value) return;
     xPos.value ??= carousel.value.offsetWidth * 0.5;
     yPos.value = carousel.value.offsetHeight * 0.1;
-    xRadius.value ??= carousel.value.offsetWidth / 2.3;
-    yRadius.value = carousel.value.offsetHeight / 6;
+    orientationXRadius.value ??= carousel.value.offsetWidth / 2.3;
+    orientationYRadius.value = carousel.value.offsetHeight / 6;
     items.splice(0, items.length);
     for (const image of Object.values(elements)) {
         image.element.removeAttribute("style");
@@ -126,17 +187,36 @@ const setupCarousel = () => {
 };
 
 const onResize = () => {
-    xRadius.value = undefined;
+    orientationXRadius.value = undefined;
     xPos.value = undefined;
     setupCarousel();
 };
 
-const goBack = () => {
+const nextSlide = () => {
     go(CarouselDirection.BACKWARDS);
+    currentItem.value--;
 };
 
-const goForward = () => {
+const previousSlide = () => {
     go(CarouselDirection.FORWARDS);
+    currentItem.value++;
+};
+
+const switchOrientation = () => {
+    isVertical.value = !isVertical.value;
+    onResize();
+};
+
+const { abortTimeout } = useAutoplay({ autoplay: props.autoplay, nextSlide: previousSlide, previousSlide: nextSlide });
+
+const onNextSlide = () => {
+    abortTimeout();
+    nextSlide();
+};
+
+const onPreviousSlide = () => {
+    abortTimeout();
+    previousSlide();
 };
 
 onMounted(() => {
@@ -162,5 +242,10 @@ onUnmounted(() => {
 .carousel-controls {
     z-index: 999;
     top: 80%;
+}
+
+.carousel-toggles {
+    z-index: 999;
+    top: calc(80% + 40px);
 }
 </style>
